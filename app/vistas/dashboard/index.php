c9e48e4e51f8a2abea37261d26849233070743a9
Aynor <espinozaleonaynor@gmail.com>
2024-09-08 05:31:55 -1200

    Actualización de index y eliminación de archivo innecesario, adición de imágenes

--- app/vistas/dashboard/index.php
+++ app/vistas/dashboard/index.php
@@ -108,6 +108,5 @@ $reservasPendientes = $resultReservasPendientes->fetch_assoc()['pendientes'];
 </script>
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
 </body>
 </html>
